b6f95595aebdef2bb4a7ac92894c06c4328eab93
PASIYAH2005 <167032076+PASIYAH2005@users.noreply.github.com>
2024-07-26 08:59:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=CMsh0Rqb#s79a11tzRj521ILl53We3BvUt_CKjn4ruY10TNXGjKk' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=ud0HGbDA#sQtUa-CE08TWzl4vgj0JqiDCLGenMgCCiPkJyKL5yNw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
